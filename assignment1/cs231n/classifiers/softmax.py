ade6eeeb0a7d85cf355f8ad258db803677a91c30
Kailian Jacy <xza1809884911@163.com>
2022-02-02 14:26:03 +0800

    finished: softmax vectorized gradient.

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -1,4 +1,5 @@
 from builtins import range
+from curses.ascii import SO
 import numpy as np
 from random import shuffle
 # from past.builtins import xrange
@@ -34,7 +35,40 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+
+    S = X.dot(W)
+    Exp_s = np.exp( (S.T - np.max(S, axis=1)).T )
+    Soft_S = Exp_s / np.sum(Exp_s, axis=1).reshape(-1,1)
+    loss = np.sum(-np.log(Soft_S[range(num_train), y])) / num_train
+
+    # Back propagation
+    dL = np.array([1/num_train]*num_train)
+    yS = np.zeros_like(Soft_S)
+    yS[range(num_train), y] = 1.0
+    dS = np.tile(dL, (num_classes, 1)).T * ( Soft_S - yS )
+    dW = X.T.dot(dS)
+
+    # # answer
+    # F = X.dot(W)
+    # exp_normalized_F = np.exp( (F.T - np.max(F, axis=1)).T )
+    # sum_i = np.sum(exp_normalized_F, axis=1)
+    
+    # p_i = exp_normalized_F[range(num_train), y] / sum_i
+    # L_i = - np.log(p_i)
+    # loss = np.sum(L_i)
+    
+    # loss /= num_train
+    # loss += reg*np.sum(W * W)
+
+    # acc_effect = (exp_normalized_F.T / sum_i).T
+    # acc_effect[range(num_train), y] -= 1.0
+    # dW = X.T.dot(acc_effect)
+
+    # dW /= num_train
+
+    dW += reg * W * 2
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -59,7 +93,24 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    s = X.dot(W)
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+
+    S = X.dot(W)
+    Exp_s = np.exp( (S.T - np.max(S, axis=1)).T )
+    Soft_S = Exp_s / np.sum(Exp_s, axis=1).reshape(-1,1)
+    loss = np.sum(-np.log(Soft_S[range(num_train), y])) / num_train
+
+
+    # Back propagation
+    dL = np.array([1/num_train]*num_train)
+    yS = np.zeros_like(Soft_S)
+    yS[range(num_train), y] = 1.0
+    dS = np.tile(dL, (num_classes, 1)).T * ( Soft_S - yS )
+    dW = X.T.dot(dS)
+
+
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
